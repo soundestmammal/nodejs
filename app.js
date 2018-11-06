2f09e221dd2925c4fdb474800b0a1498c55f2113
Robert Checco <robert.checco@gmail.com>
2018-11-05 23:48:28 -0500

    This should use a Switch statement

--- app.js
+++ app.js
@@ -4,3 +4,22 @@ const fs = require('fs');
 const _ = require('lodash');
 
 const notes = require('./notes');
+
+const command = process.argv[2];
+console.log(process.argv);
+
+if (command === 'add') {
+    console.log('Adding new note');
+}
+else if (command === 'remove'){
+    console.log('Remove the note');
+}
+else if (command === 'read'){
+    console.log('Fetching all notes');
+}
+else if (command === 'list'){
+    console.log('Listing all notes');
+}
+else {
+    console.log('command not recognized.');
+}
