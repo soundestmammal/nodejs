795fa33935eeff5fea0bc20edbf7fd59eec60a23
Robert Checco <robert.checco@gmail.com>
2018-11-14 00:28:16 -0500

    Create Boilerplate before refactor

--- react/jsx/src/index.js
+++ react/jsx/src/index.js
@@ -32,11 +32,21 @@ const App = () => {
     }
 
     return (
-        <div>
-            <label htmlFor="name">Enter Name:</label>
-            <input id="name" type="text" />
-            <button style={style.button}> {click()} </button>
-            {list()}
+        <div className="ui container comments">
+            <div className="comment">
+                <a href="/" className="avatar">
+                    <img alt="avatar" />
+                </a>
+                <div className="content">
+                    <a href="/" className="author">
+                        Sam
+                    </a>
+                    <div className="metadata">
+                        <span className="data">Today at 6:00PM</span>
+                    </div>
+                    <div className="text">Nice blog post!</div>
+                </div>
+            </div>
         </div> );
 }
 
