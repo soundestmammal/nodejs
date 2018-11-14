34f031c74d0409d2f18d53580d9e733c5c71b138
Robert Checco <robert.checco@gmail.com>
2018-11-14 01:19:59 -0500

    Refactor CommentDetail with props and child component

--- react/jsx/src/index.js
+++ react/jsx/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import faker from 'faker';
+import CommentDetail from './CommentDetail';
 
 // class App extends React.Component {
 //     render() {
@@ -8,58 +9,55 @@ import faker from 'faker';
 //     }
 // }
 
-const click = () => {
-    const clickMe = "Click";
-    return clickMe;
-}
 
 
 
 const App = () => {
-    const list = () => {
-        return (
-            <div style={{'display': 'flex', 'flexDirection': 'column', 'height': '3000px', 'width': '300px', 'border': '3 px solid blue', 'justifyContent': 'spaceAround'}}>
-                <div style={{'backgroundColor': 'green'}}>
-                    <div image={style.image}>hi</div>
-                </div>
-                <div style={{'backgroundColor': 'green'}}>
-                    <div image={style.image}>hi</div>
-                </div>
-                <div style={{'backgroundColor': 'green'}}>
-                    <div image={style.image}>hi</div>
-                </div>
-            </div>
-        );
-    }
+        // return (
+        //     <div style={{'display': 'flex', 'flexDirection': 'column', 'height': '3000px', 'width': '300px', 'border': '3 px solid blue', 'justifyContent': 'spaceAround'}}>
+        //         <div style={{'backgroundColor': 'green'}}>
+        //             <div image={style.image}>hi</div>
+        //         </div>
+        //         <div style={{'backgroundColor': 'green'}}>
+        //             <div image={style.image}>hi</div>
+        //         </div>
+        //         <div style={{'backgroundColor': 'green'}}>
+        //             <div image={style.image}>hi</div>
+        //         </div>
+        //     </div>
+        // );
 
     return (
         <div className="ui container comments">
-            <div className="comment">
-                <a href="/" className="avatar">
-                    <img src={faker.image.avatar()} />
-                </a>
-                <div className="content">
-                    <a href="/" className="author">
-                        Sam
-                    </a>
-                    <div className="metadata">
-                        <span className="data">Today at 6:00PM</span>
-                    </div>
-                    <div className="text">Nice blog post!</div>
-                </div>
-            </div>
+            <CommentDetail 
+                author="Kenny Liao"
+                text={faker.lorem.sentences()}
+                avatar={faker.image.avatar()}
+            />
+            <CommentDetail
+                author="Dave Vitt"
+                text={faker.lorem.sentences()}
+                avatar={faker.image.avatar()}
+
+            />
+            <CommentDetail
+                author="Rick Weber"
+                text={faker.lorem.sentences()}
+                avatar={faker.image.avatar()}
+
+            />
         </div> );
 }
 
-const style = {
-    button: {
-        'backgroundColor': 'blue',
-        'color': 'white'
-    },
-    image: {
-        'backgroundColor': 'green',
-    }
-}
+// const style = {
+//     button: {
+//         'backgroundColor': 'blue',
+//         'color': 'white'
+//     },
+//     image: {
+//         'backgroundColor': 'green',
+//     }
+// }
 ReactDOM.render(
     <App />,
     document.querySelector('#root')
