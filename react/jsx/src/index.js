d548aacc449243fc2165651cf31d7b4d25fd161f
Robert Checco <robert.checco@gmail.com>
2018-11-14 00:34:42 -0500

    Add Image using faker from npm
    This is pretty cool how you can use this all for boilerplate. Nice library for production

--- react/jsx/src/index.js
+++ react/jsx/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import faker from 'faker';
 
 // class App extends React.Component {
 //     render() {
@@ -35,7 +36,7 @@ const App = () => {
         <div className="ui container comments">
             <div className="comment">
                 <a href="/" className="avatar">
-                    <img alt="avatar" />
+                    <img src={faker.image.avatar()} />
                 </a>
                 <div className="content">
                     <a href="/" className="author">
