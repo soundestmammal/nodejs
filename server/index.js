5e1126842ed4b0bc38956d87c7ff9fe13c243989
Robert Checco <robert.checco@gmail.com>
2018-11-18 03:17:59 -0500

    Set up mongodb, connected OAuth and use mongoose create collection in db

--- server/index.js
+++ server/index.js
@@ -1,5 +1,11 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const keys = require("./config/keys");
+require("./models/user");
 require("./services/passport");
+
+
+mongoose.connect(keys.mongoURI);
 
 const app = express();
 
--- server/services/passport.js
+++ server/services/passport.js
@@ -1,6 +1,9 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
 const keys = require('../config/keys');
+
+const User = mongoose.model('users');
 
 passport.use(
     new GoogleStrategy(
@@ -10,8 +13,16 @@ passport.use(
         callbackURL: '/auth/google/callback'
     },
     (accessToken, refreshToken, profile, done) => {
-        console.log('accessToken', accessToken);
-        // console.log('refreshToken', refreshToken);
-        // console.log('profile', profile);
+        User.findOne({ googleId: profile.id })
+            .then((existingUser) => {
+                if (existingUser) {
+                    // we already have a record with the given profile id
+                }
+                else {
+                    // we don't have a user with that profile id so lets make one!
+                    new User({ googleId: profile.id }).save();
+                }
+            })
+        
     }
 ));
