f28a614b8a78e85e0a28f3d3ba2cb90e2fc94586
Robert Checco <robert.checco@gmail.com>
2018-11-27 10:00:14 -0500

    Beginning to review Webpack concepts

---
+++ react/blogs/src/components/PostList.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class PostList extends Component {
+    render() {
+        return <div>Post List made it dude!!!</div>;
+    }
+}
+
+export default PostList;
---
+++ react/js_modules/src/sum.js
@@ -0,0 +1,5 @@
+const sum = (a,b) => a + b;
+// How will we link this file with other files in our project?
+// Commonjs
+// AMD
+// ES2015
