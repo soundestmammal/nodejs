5c8cbbb456a006261779c387bbd503429c9317f3
Robert Checco <robert.checco@gmail.com>
2019-01-09 23:56:20 -0500

    Map helper in ES6

---
+++ jsconcepts/map.js
@@ -0,0 +1,8 @@
+var numbers = [1,2,3];
+var doubledNumbers = [];
+
+for (var i = 0; i < numbers.length; i++) {
+    doubledNumbers.push(number[i] * 2);
+}
+
+var doubles = numbers.map((number)=> number * 2)
