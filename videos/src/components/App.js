f0361a3581a3ba49cb3b63e1680eeda758395f26
Robert Checco <robert.checco@gmail.com>
2019-01-26 01:17:23 -0500

    Add SearchBar Component to project

--- videos/src/components/App.js
+++ videos/src/components/App.js
@@ -1,8 +1,15 @@
 import React from 'react';
 
+import SearchBar from './SearchBar';
+
 class App extends React.Component {
     render() {
-        return <h1> This is here</h1>
+        return (
+        <div>
+            <h1> This is here</h1>
+            <SearchBar />
+        </div>
+        );
     }
 }
 export default App;
---
+++ videos/src/components/SearchBar.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class SearchBar extends React.Component {
+    render() {
+        return <div>Searchbar</div>
+    }
+}
+
+export default SearchBar;
